7ba8da5cdd6d1d4b525c4d9236c61defd2a547ff
oladegs <144366155+oladegs@users.noreply.github.com>
2023-10-07 19:54:24 -0400

    added the version of node in my package.json and commented my app.js Well

--- Assignment1/config/app.js
+++ Assignment1/config/app.js
@@ -1,48 +1,56 @@
-// all the piece part that make the whole express generator experience
+// This is  a setup for an Express.js application. It includes the importation of necessary packages, configuration of middleware, and routing for handling HTTP requests and rendering views
+//it contains all  all the piece part that make the whole express generator experience
 // Installed third party packages
-let createError = require("http-errors");
-let express = require("express"); // dl
-let path = require("path");
-let cookieParser = require("cookie-parser"); // dl
-let logger = require("morgan"); // dl
 
-// two routes for our main file and user file
+// Importing different packages
+let createError = require("http-errors");
+let express = require("express");
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let logger = require("morgan");
+
+// Import two custom route files for the main application and user-specific routes.
 let indexRouter = require("../app/routes/index");
 let usersRouter = require("../app/routes/users");
 
 // Creates an instance of the express application and stores a reference in the app variable
 let app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "../app/views")); // configure view /view engine to the app ; read more on MVC
-// Module is the way we connect to our logic , controller is logic behind our routes, and view are the end pages
-app.set("view engine", "ejs"); // npm express -e when generating a b=view
+// Configure the view engine and views directory for rendering dynamic content.
+app.set("views", path.join(__dirname, "../app/views")); // configure view /view engine to the app ;
+app.set("view engine", "ejs"); // npm express -e when generating a view
 
-// Series of activations
+// Series of middleware activations for various functionalities.
+// `app.use` is a method used to mount middleware functions in an Express application. Middleware functions are functions that have access to the request object (`req`), the response object (`res`), and the `next` function in the application's request-response cycle.
+
 app.use(logger("dev"));
-app.use(express.json()); // data exchange
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "../public"))); // static routes to access public folder for  the client side
-app.use(express.static(path.join(__dirname, "../node_modules")));
+app.use(express.static(path.join(__dirname, "../public"))); // Serve static files from the 'public' directory for client-side assets.
+app.use(express.static(path.join(__dirname, "../node_modules"))); // Serve static files from the 'node_modules' directory.
+
+// Define route handlers for the main application ("/") and user-specific ("/users") routes.
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
+// Middleware for handling 404 errors (resource not found).
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handling middleware for handling errors.
 app.use(function (err, req, res, next) {
   console.log(err);
-  // set locals, only providing error in development
+
+  // Set locals for error messages, providing error details in development mode.
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  //in the event of an error we can render the error page
+  // Render the error page with an appropriate HTTP status code (default to 500).
   res.status(err.status || 500);
   res.render("error", { title: "URL/na" });
 });
 
+// Export the 'app' instance to make it available for other parts of the application.
 module.exports = app;
